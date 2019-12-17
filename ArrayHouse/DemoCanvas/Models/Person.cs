ff83ed0b2f89254301e1a679ede92bc4ecd2cd39
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-12-17 19:48:03 +0200

    Add solutions

---
+++ ArrayHouse/DemoCanvas/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Media.Animation;
+
+namespace DemoCanvas.Views
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public Person()
+        {
+
+        }
+    }
+}
