eeebd9d60e0013a7582cd1ef9ac2c1fd43bb295e
Borislav Valkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-12-19 17:33:38 +0200

    Add solution

--- ArrayHouse/HouseDemoMoving/MainWindow.xaml.cs
+++ ArrayHouse/HouseDemoMoving/MainWindow.xaml.cs
@@ -1,93 +1,75 @@
-﻿using System.Threading;
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Threading;
 
 namespace HouseDemoMoving
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private StackPanel stackPanel;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext = House;
-
-            //Thread thread = new Thread(new ThreadStart(ChangePosition));
-            //thread.Start();
         }
 
-        public House House = new House 
+        StackPanel StackPanel
         {
-            Number = 1,
-            XPosition = 0,
-            YPosition = 0,
-        };
-
-        public void ChangePosition()
-        {
-            for (int i = 0; i < 1000; i++)
+            get
             {
-                House.YPosition += 0.1;
-                Thread.Sleep(1);
+                return this.stackPanel;
+            }
+            set
+            {
+                this.stackPanel = value;
+                NotifyPropertyChanged();
             }
         }
+
+
 
         private void CreateNewArrayHouse(object sender, RoutedEventArgs e)
         {
-            House house = new House
-            {
-                Number = 1,
-                XPosition = 0,
-                YPosition = 0,
-            };
+            MainCanvas.Children.Clear();
 
-            Canvas canvas = new Canvas();
-
-            int xPosition = 0;
+            int n = 0;
 
             for (int i = 0; i < 10; i++)
             {
-                StackPanel stackPanel = new StackPanel();
-                canvas.Children.Add(stackPanel);
+                TextBlock textBlock = new TextBlock();
+                textBlock.Text = "Test";
 
-                stackPanel.Children.Add(new TextBlock { Text = house.Number.ToString() });
+                this.StackPanel = new StackPanel();
+                StackPanel.Children.Add(textBlock);
 
-                Canvas.SetLeft(stackPanel, xPosition);
-                xPosition += 100;
+                Canvas.SetLeft(StackPanel, n);
+
+                MainCanvas.Children.Add(StackPanel);
+
+                n += 50;
             }
-
-            MainGrid.Children.Add(canvas);
         }
 
-        private void Execute(object sender, RoutedEventArgs e)
+        private void Move(object sender, RoutedEventArgs e)
         {
-            House house = new House
-            {
-                Number = 1,
-                XPosition = 0,
-                YPosition = 0,
-            };
-
-            Canvas canvas = new Canvas();
-
-            double xPosition = house.XPosition;
-
-            for (int i = 0; i < 10; i++)
-            {
-                StackPanel stackPanel = new StackPanel();
-                canvas.Children.Add(stackPanel);
-
-                stackPanel.Children.Add(new TextBlock { Text = house.Number.ToString() });
-
-                Canvas.SetLeft(stackPanel, xPosition);
-                xPosition += 100;
-            }
-
             for (int i = 0; i < 100; i++)
             {
-                house.YPosition = i;
+                Canvas.SetTop(StackPanel, i);
+                Thread.Sleep(10);
+                this.UpdateLayout();
             }
+        }
 
-            MainGrid.Children.Add(canvas);
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
